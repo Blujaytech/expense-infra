f3a1581ec7fad6335b0d63611bcb5362ae3bbb6e
Blujaytech <blujaytechnlogies@gmail.com>
2026-05-08 15:42:21 +0530

    Change VPC module source to local path

--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/Blujaytech/expense-infra.git"
+  source = "../modules/vpc"
 
   vpc_cidr              = var.vpc_cidr
   project_name          = var.project_name
@@ -10,4 +10,3 @@ module "vpc" {
   database_subnet_cidrs = var.database_subnet_cidrs
   is_peering_required   = var.is_peering_required
 }
-
